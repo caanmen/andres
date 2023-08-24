400072b69262e7e154f7ad1c00beeb612fc79006
Andres Mendez <camendez632@ucatolica.edu.co>
2023-08-23 21:02:24 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/solid/Solid.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solid;
+
+/**
+ *
+ * @author sala4
+ */
+public class Solid {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
